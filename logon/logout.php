ed2c13fe2ad3e6a05aff21d0838a1d428db9b7a3
Noel Magalhaes <nlnoel.cnb@gmail.com>
2019-02-24 23:09:31 -0300

    Corrigindo bug do logout

--- logon/logout.php
+++ logon/logout.php
@@ -3,7 +3,6 @@
 session_start();
 session_destroy();
 
-print "<script>alert('Já vai? que pena... então, até a próxima!!');</script>";
-header("location: logon/logon.php");
+header("location: logon.php");
 
 ?>
