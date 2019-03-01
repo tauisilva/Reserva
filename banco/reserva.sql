7a35ae544b7baf3dd7cd5f1c110cdf9fe27e2e4a
Noel Magalhaes <nlnoel.cnb@gmail.com>
2019-03-01 00:13:25 -0300

    Adicionando atualização do banco de dados

--- banco/reserva.sql
+++ banco/reserva.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 23-Fev-2019 às 04:16
+-- Generation Time: 01-Mar-2019 às 03:11
 -- Versão do servidor: 5.7.21
 -- PHP Version: 5.6.35
 
@@ -36,19 +36,23 @@ CREATE TABLE IF NOT EXISTS `funcionario` (
   `nome_funcionario` varchar(40) NOT NULL,
   `usuario_funcionario` varchar(40) NOT NULL,
   `CPF_funcionario` varchar(40) NOT NULL,
-  `cidade` varchar(40) DEFAULT NULL,
-  `estado` char(2) DEFAULT NULL,
   `email` varchar(50) NOT NULL,
   `senha_funcionario` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`idfuncionario`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `funcionario`
+--
+
+TRUNCATE TABLE `funcionario`;
 --
 -- Extraindo dados da tabela `funcionario`
 --
 
-INSERT INTO `funcionario` (`idfuncionario`, `nome_funcionario`, `usuario_funcionario`, `CPF_funcionario`, `cidade`, `estado`, `email`, `senha_funcionario`) VALUES
-(1, 'Noel Lucas Cunha Magalhães', 'Nlnoel', '035.314.431-27', 'Brasília', 'DF', 'nlnoel.cnb@gmail.com', '202cb962ac59075b964b07152d234b70');
+INSERT INTO `funcionario` (`idfuncionario`, `nome_funcionario`, `usuario_funcionario`, `CPF_funcionario`, `email`, `senha_funcionario`) VALUES
+(1, 'Noel Lucas Cunha Magalhães', 'Nlnoel', '035.314.431-27', 'nlnoel.cnb@gmail.com', '202cb962ac59075b964b07152d234b70'),
+(14, 'Jose Tenta', 'JsTenta', '789.456.123-10', 'exemplo@exemplo.com', '250cf8b51c773f3f8dc8b4be867a9a02');
 
 -- --------------------------------------------------------
 
@@ -68,11 +72,16 @@ CREATE TABLE IF NOT EXISTS `hora_reserva` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Truncate table before insert `hora_reserva`
+--
+
+TRUNCATE TABLE `hora_reserva`;
+--
 -- Extraindo dados da tabela `hora_reserva`
 --
 
 INSERT INTO `hora_reserva` (`salas_idsalas`, `funcionario_idfuncionario`, `hora_inicial`, `hora_final`, `dia_reserva`) VALUES
-(8, 10, '01:00:00', '02:00:00', '2018-12-01');
+(8, 1, '01:00:00', '02:00:00', '2018-12-01');
 
 -- --------------------------------------------------------
 
@@ -86,8 +95,13 @@ CREATE TABLE IF NOT EXISTS `salas` (
   `nome_sala` varchar(50) NOT NULL,
   `numero_sala` int(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`idsalas`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `salas`
+--
+
+TRUNCATE TABLE `salas`;
 --
 -- Extraindo dados da tabela `salas`
 --
@@ -101,7 +115,8 @@ INSERT INTO `salas` (`idsalas`, `nome_sala`, `numero_sala`) VALUES
 (6, 'SalaExemplo', 9),
 (7, 'sala10', 10),
 (8, 'sala20', 20),
-(9, 'teste500', 500);
+(9, 'teste500', 500),
+(10, 'sala 1', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
